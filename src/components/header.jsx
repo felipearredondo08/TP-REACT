a1234c45725a4c71eab5583a49e7f0a973c0c04e
Felipe Arredondo <hellvilleskateshop@gmail.com>
2023-11-21 19:05:47 -0300

    tercer commit

---
+++ src/components/header.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react';
+import './Header.css';
+import logo from '../assets/pildora.png';
+
+const Header = () => {
+  const [logoLoaded, setLogoLoaded] = useState(false);
+
+  useEffect(() => {
+    const img = new Image();
+    img.src = logo;
+
+    img.onload = () => {
+      setLogoLoaded(true);
+    };
+  }, [logo]);
+
+  return (
+    <div className={`header ${logoLoaded ? 'logo-loaded' : ''}`}>
+      <img src={logo} alt="Logo de la marca" className="logo" id="logo" />
+      <h1>mNEMO</h1>
+    </div>
+  );
+};
+
+export default Header;
